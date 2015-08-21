2cc2f14767dcb9f3702f48ab3cabb4a2d37bd077
miki89 <mikolajheller@gmail.com>
2015-08-21 13:45:52 +0200

    Adding users model

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'simple_form', '~> 3.1.0'
+gem 'bootstrap-sass', '~> 3.3.5.1'
+gem 'devise', '~> 3.5.2'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
+
+	def index	
+		@books = Book.all.order("created_at DESC")
+	end
+	
+	def show
+		
+	end
+	
+	def new
+	@book = current_user.books.build
+	end
+	
+	def create
+	@book = current_user.books.build(book_params)
+	
+		if @book.save
+			redirect_to action: "index"
+		else 
+			render 'new'
+		end
+	end
+	
+	def edit
+	end
+	
+	def update
+		if @book.update(book_params)
+			redirect_to book_path(@book)
+		else	
+			render 'edit'
+		end	
+	end
+	
+	def destroy
+	
+		@book.destroy
+		redirect_to action: "index"
+		
+	end
+	
+	private
+		def book_params
+			params.require(:book).permit(:title, :description, :author)
+		end
+		
+		def find_book
+			@book = Book.find(params[:id])
+		end
+end
